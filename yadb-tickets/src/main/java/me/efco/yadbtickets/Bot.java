2e34d5774591c5cf9af5312dfd6286f7cddaf351
EFCO <emilfco@icloud.com>
2022-05-18 12:54:53 +0200

    Bot now starts with api_token from config.properties

--- yadb-tickets/src/main/java/me/efco/yadbtickets/Bot.java
+++ yadb-tickets/src/main/java/me/efco/yadbtickets/Bot.java
@@ -1,13 +1,23 @@
 package me.efco.yadbtickets;
 
+import me.efco.yadbtickets.data.ConfigLoader;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
+import javax.security.auth.login.LoginException;
+
 public class Bot {
     private static JDA api;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
+        api = JDABuilder.createDefault(ConfigLoader.getInstance().loadConfigByName("api_token"))
+                .setActivity(Activity.playing("/help"))
+                .build();
 
+        //This is so we can make sweet sweet slash commands
+        api.setRequiredScopes("identify", "bot", "webhook.incoming", "applications.commands");
+
+        System.out.println("YADB Tickets has successfully started. Invite via " + api.getInviteUrl());
     }
 }
