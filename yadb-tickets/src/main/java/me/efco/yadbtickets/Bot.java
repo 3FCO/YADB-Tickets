61635d3482c462f557fe53d7f56b9885365cb321
EFCO <emilfco@icloud.com>
2022-05-18 12:45:44 +0200

    Default layout

---
+++ yadb-tickets/src/main/java/me/efco/yadbtickets/Bot.java
@@ -0,0 +1,13 @@
+package me.efco.yadbtickets;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class Bot {
+    private static JDA api;
+
+    public static void main(String[] args) {
+
+    }
+}
